b6acc9145dbecd9e4c16a7785f9cfbf3daa266d6
linear14 <47495592+linear14@users.noreply.github.com>
2020-07-13 17:06:19 +0900

    Preview previous or next items
    - setPagerTransformer
    - Get screenWidth (resources.displayMetrics.widthPixels)

--- app/src/main/java/com/dongldh/tabandviewpager2ex/fragment/BFragment.kt
+++ app/src/main/java/com/dongldh/tabandviewpager2ex/fragment/BFragment.kt
@@ -20,13 +20,19 @@ class BFragment: Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val view = inflater.inflate(R.layout.fragment_b, container, false)
 
-        activity?.viewPager2?.requestDisallowInterceptTouchEvent(false)
+        val pageMarginPixel = resources.getDimensionPixelOffset(R.dimen.pageMargin)
+        val pagerWidth = resources.getDimensionPixelOffset(R.dimen.pagerWidth)
+        val screenWidth = resources.displayMetrics.widthPixels
+        val offsetPixel = screenWidth - pagerWidth - pageMarginPixel
 
         bookList = BookDao(this.requireContext()).selectAll()
         view.recyclerViewPager2.adapter = FragmentBAdapter(bookList)
-        view.recyclerViewPager2.orientation = ViewPager2.ORIENTATION_VERTICAL
+        view.recyclerViewPager2.orientation = ViewPager2.ORIENTATION_HORIZONTAL
+        view.recyclerViewPager2.offscreenPageLimit = 1
+        view.recyclerViewPager2.setPageTransformer { page, position -> page.translationX = position * (-offsetPixel) }
+
+        activity?.viewPager2?.requestDisallowInterceptTouchEvent(true)
 
         return view
     }
-
 }
